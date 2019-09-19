28b4703162693b7fbd3b3ebd856e22c9e8db0841
yamert89 <softoad2@gmail.com>
2019-09-19 15:12:00 +0300

    webresConfig

--- src/main/java/esn/configs/AppConfig.java
+++ src/main/java/esn/configs/AppConfig.java
@@ -1,15 +1,9 @@
 package esn.configs;
 
-import esn.viewControllers.accessoryFunctions.MyStaticHandler;
 import esn.viewControllers.accessoryFunctions.SessionUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class AppConfig {
@@ -26,16 +20,5 @@ public class AppConfig {
         return new SessionUtil();
     }
 
-    @Bean
-    WebMvcConfigurer configurer(){
-        return new WebMvcConfigurer() {
-            @Override
-            public void addResourceHandlers(ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("/resources/**")
-                        .addResourceLocations("/resources/")
-                        .resourceChain(true)
-                        .addResolver(new MyStaticHandler());
-            }
-        };
-    }
+
 }
---
+++ src/main/java/esn/configs/WebResourcesConfig.java
@@ -0,0 +1,25 @@
+package esn.configs;
+
+import esn.viewControllers.accessoryFunctions.MyStaticHandler;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "esn.viewControllers")
+public class WebResourcesConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        System.out.println("___________________________REGISTRY");
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/")
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new MyStaticHandler());
+
+    }
+}
