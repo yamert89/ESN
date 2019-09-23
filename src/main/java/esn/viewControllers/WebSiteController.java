0eba4c7870f291a6f17bd011ca0111f04b153014
yamert89 <softoad2@gmail.com>
2019-09-23 16:53:45 +0300

    remove email service

--- src/main/java/esn/viewControllers/WebSiteController.java
+++ src/main/java/esn/viewControllers/WebSiteController.java
@@ -1,21 +1,11 @@
 package esn.viewControllers;
 
-import esn.services.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebSiteController {
 
-    private EmailService emailService;
-
-    @Autowired
-    @Qualifier("adminEmailService")
-    public void setEmailService(EmailService emailService) {
-        this.emailService = emailService;
-    }
 
     @GetMapping(value = "/")
     public String index(){
