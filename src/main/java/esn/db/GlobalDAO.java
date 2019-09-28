e6307b248a0784d2f339230780abd4d7d4a9cc82
Пендальф Синий <softoad2@gmail.com>
2019-09-28 11:22:38 +0300

    - url in staff added - profile bag fixed

--- src/main/java/esn/db/GlobalDAO.java
+++ src/main/java/esn/db/GlobalDAO.java
@@ -81,7 +81,7 @@ public class GlobalDAO implements InitializingBean {
             txManager.commit(ts);
         }catch (Exception e){
             txManager.rollback(ts);
-            logger.debug("init db", e);
+            logger.debug("Ошибка стартовой инициализации таблиц");
         }
 
     }
