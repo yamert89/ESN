ac3b8de7cf2c3cf336cff029a3874fabcf9c3fc5
yamert89 <softoad2@gmail.com>
2019-10-02 10:15:14 +0300

    - css excludes - index bag fixed

--- src/main/webapp/resources/index/index.js
+++ src/main/webapp/resources/index/index.js
@@ -1,4 +1,7 @@
 var arrow = {};
+if (sessionStorage.getItem('scroll') == '1') {
+    document.body.style.overflowY = 'auto';
+}
 
 var els = document.getElementsByClassName("wrapper");
 els[0].onmouseover = lets;
@@ -13,8 +16,6 @@ function lets(){
         f2 = temp;
 
     }
-
-    //var go = this.nextSibling.nextSibling;
     f1.nextSibling.nextSibling.classList.remove("lets_go");
     f1.nextSibling.nextSibling.classList.add("lets_go_showed");
     f2.nextSibling.nextSibling.classList.remove("lets_go_showed");
@@ -86,85 +87,8 @@ function hideTree(element) {
 }
 
 function scroll() {
+    sessionStorage.setItem('scroll', '1');
     document.body.style.overflowY = "auto";
     document.getElementsByClassName("features")[0].scrollIntoView({behavior : "smooth", block: "start", inline: "nearest"});
     arrow.style.display = "none";
 }
-
-
-
-
-
-
-
-
-
-
-/*
-$(document).ready(function () {
-    $(".wrapper").hover(function () {
-        var showed = $(".lets_go_showed");
-        showed.removeClass("lets_go_showed");
-        showed.addClass("lets_go");
-        var go = $(this).next();
-        go.removeClass("lets_go");
-        go.addClass("lets_go_showed");
-    });
-
-    $(".lets_go").click(function () {
-        location.href = $(this).attr("id") === "cloud" ? "/notice_cloud" : "/notice_local";
-    });
-    /!*var ex1 = $("#ex1");
-    var ex2 = $("#ex2");
-    ex1.attr("src", ex1.attr("data-src"));
-    ex2.attr("src", ex2.attr("data-src"));*!/
-    arrow = $(".arrow");
-
-    setTimeout(function () {
-        $("body").css("overflow-y", "auto");
-        if (window.pageYOffset > 0) return;
-        arrow.attr("src", arrow.attr("data-src"));
-        arrow.css("display", "block");
-    }, 2000);
-    arrow.click(scroll);
-    $(window).scroll(scroll);
-
-    $(".q").click(function () {
-        var question = $(this).next();
-        if (question.hasClass("hidden")) {
-            showTree(question);
-            $(document).scrollTop(9999);
-        }
-        else hideTree(question);
-    });
-
-    $(".send").click(function () {
-        var email = $(".email").val();
-        var text = $("textarea").val();
-        $.post("/question", {message : text, email : email});
-        hideTree($(".question"));
-    })
-
-
-
-});
-
-function showTree(element) {
-    element.removeClass("hidden");
-    var children = element.children();
-    if (children.length > 0) showTree(children);
-
-}
-
-function hideTree(element) {
-    element.addClass("hidden");
-    var children = element.children();
-    if (children.length > 0) hideTree(children);
-}
-
-function scroll() {
-    $(window).unbind("scroll", scroll);
-    //window.scrollTo(0, $("#header").height() + 2);
-    $('html, body').animate({ scrollTop: $(".office").height() + 2 }, 600);
-    arrow.css("display", "none");
-}*/
